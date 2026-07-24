954c0ebc4241a3e87f3fd61ab32d431867579a1e
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-07-24 10:37:40 +0600

    add middle value of the linked list

---
+++ Linked List/876.Middle of the Linked List.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int size (ListNode* head){
+    ListNode* temp= head;
+    int cnt =0;
+    while(temp!= NULL){
+        cnt++;
+        temp=temp->next;
+    }
+    return cnt;
+}
+    ListNode* middleNode(ListNode* head) {
+        int sz = size(head);
+        ListNode* temp= head;
+        for(int i=0; i<sz/2; i++){
+            temp= temp->next;
+        }
+        return temp;
+    }
+};
